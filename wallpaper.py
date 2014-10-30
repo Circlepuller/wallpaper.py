7bede8fce02ee87c7157f6d6bb747a772856c7f4
Dan Saunders <dsaunders@dansaunders.me>
2014-10-30 01:14:43 -0400

    Fixed a bug!

--- wallpaper.py
+++ wallpaper.py
@@ -4,32 +4,24 @@
 # By Dan Saunders
 # Pulls a beautiful wallpaper for each X output, correct resolution and all
 
-# Setting this to true enables terminal debugging output
-DEBUG = True
-
-# Choose between 'feh', 'xfce', 'cinnamon', or 'gnome'
-DESKTOP = 'xfce'
-
+from glob import glob
 from os import makedirs
 from os.path import dirname, exists, realpath
 from random import randint
-from re import I, findall
+from re import I, findall, sub
 from subprocess import check_output
 from time import sleep
 from urllib.request import Request, urlopen
 
-USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1)'
+USER_AGENT = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
 
 def get_wallpaper(display, resolution):
 	request = Request('http://interfacelift.com/wallpaper/downloads/random/fullscreen/{}/'.format(resolution))
 	request.add_header('User-Agent', USER_AGENT)
 
-	if DEBUG: print('Searching "{}" for wallpapers'.format(request.full_url))
-
 	try:
 		request = Request('http://interfacelift.com{}'.format([match for match in findall(r'(/wallpaper/[a-z0-9]+/[a-z0-9_]+\.jpg)', urlopen(request).read().decode('utf-8'), I) if not match.startswith('/wallpaper/preview')][0]))
-	except
-		if DEBUG: print('Could not fetch wallpaper')
+	except:
 		exit()
 
 	request.add_header('User-Agent', USER_AGENT)
@@ -40,8 +32,6 @@ def get_wallpaper(display, resolution):
 	if not exists(path):
 		makedirs(path)
 
-	if DEBUG: print('Saving wallpaper "{}" as "{}"'.format(request.full_url, path + filename))
-
 	f = open(path + filename, 'w+b')
 	f.write(urlopen(request).read())
 	f.close()
@@ -49,21 +39,27 @@ def get_wallpaper(display, resolution):
 	return path + filename
 
 def get_displays():
-	displays = check_output('xrandr | grep -e "\\bconnected" | cut -d" " -f1', shell=True).decode('utf-8').strip().split('\n')
-	resolutions = check_output('xrandr | grep "\*" | cut -d" " -f4', shell=True).decode('utf-8').strip().split('\n')
+	displays = {}
+	files = glob('/sys/class/drm/card*/modes')
+	devices = [sub(r'^.*card[0-9]-([a-z]+)(-I)?-([0-9]+)/modes$', r'\1\3', device_file, flags=I) for device_file in files]
 	
-	return dict(zip(displays, resolutions))
+	for index in range(len(files)):
+		resolutions = open(files[index], 'r').read().strip()
+		
+		if resolutions:
+			displays[devices[index]] = resolutions.split()[0]
+	
+	return displays
 
 if __name__ == '__main__':
-	displays = get_displays().items()
-	wallpapers = [get_wallpaper(display, resolution) for (display, resolution) in displays]
+	# Uncomment if you use XFCE
+	# TODO: Proper multi-monitor support
+	check_output(['xfconf-query', '-c', 'xfce4-desktop', '--property', '/backdrop/screen0/monitor0/image-path', '--set', [get_wallpaper(display, resolution) for (display, resolution) in get_displays().items()][0]]).decode('utf-8')
 
-	if DESKTOP == 'xfce':
-		# TODO: Proper multi-monitor support
-		check_output(['xfconf-query', '-c', 'xfce4-desktop', '--property', '/backdrop/screen0/monitor0/image-path', '--set', wallpapers[0]]).decode('utf-8')
-	elif DESKTOP == 'cinnamon':
-		# TODO: Multi-monitor support
-		check_output(['gsettings', 'set', 'org.cinnamon.desktop.background', 'picture-uri', 'file://{}'.format(wallpapers[0])])
-	elif DESKTOP == 'feh':
-		check_output(['feh', '--bg-scale'] + wallpapers).decode('utf-8')
+	# Uncomment if you use Cinnamon
+	# TODO: Multi-monitor support
+	#check_output(['gsettings', 'set', 'org.cinnamon.desktop.background', 'picture-uri', 'file://{}'.format([get_wallpaper(display, resolution) for (display, resolution) in get_displays().items()][0])])
+
+	# Uncomment if you use feh (used in smaller window managers)
+	#check_output(['feh', '--bg-scale'] + [get_wallpaper(display, resolution) for (display, resolution) in get_displays().items()]).decode('utf-8')
 
